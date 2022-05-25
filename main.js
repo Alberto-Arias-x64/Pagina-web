49673bab152b005f2e020ed8a098d23d3a7c6a18
x64 <alberto_arias_x64@gmail.com>
2022-05-25 00:51:01 -0500

    ready to deploy

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const slider = document.querySelector(".images")
-let clickscroll = 320
-let scroll_amount = 0
-
-function slider_scroll_left(){
-
-    slider.scrollTo({
-        left: (scroll_amount -= clickscroll),
-        behavior: 'auto'
-    })
-    if(scroll_amount <0){
-        scroll_amount = 0
-    }
-}
-
-function slider_scroll_right(){
-    if(scroll_amount <= slider.scrollWidth - slider.clientWidth){
-        slider.scrollTo({
-            left: (scroll_amount += clickscroll),
-            behavior: 'auto'
-        })
-    }
+function redir(link){
+    window.open(link, '_blank');
 }
